0525ea366fd6973b78eee3a349311ed0a9c74786
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-08-04 09:53:16 -0400

    commit tree 77011bdf99242d55246c831657a546515bdd73b5

---
+++ app.rb
@@ -0,0 +1,21 @@
+#puts "Hello world!"
+#puts "Hello world!"
+#puts "Hello world!" 
+
+#print "Hello world!"
+#print "Hello world!"
+#print "Hello world!" 
+
+#p [1, 2, 3] 
+
+#pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }] 
+
+#it 'outputs the string "Pass this test, please." using the print method' do
+  #expect { load 'app.rb' }.to output(a_string_including("Pass this test, please.")).to_stdout
+#end 
+
+puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1, 2, 3]
